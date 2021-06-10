999beeb1e1881b966d7a8a99e43f03386868c9bb
bernard-david <dpbernard18@gmail.com>
2021-06-10 12:46:47 -0400

    adjusted routes to handle conversions

--- routes.py
+++ routes.py
@@ -6,15 +6,15 @@ def hello_world():
 @app.route('/dojo')          
 def dojo():
     return 'Dojo!'
-@app.route('/say/<name>')
+@app.route('/say/<string:name>')
 def say(name):
     print(name)
-    return "Hi, " + str(name)
-@app.route('/repeat/<num>/<word>')
+    return "Hi, " + name
+@app.route('/repeat/<int:num>/<string:word>')
 def repeat(num, word):
     print(num)
     print(word)
-    return int(num) * str(word)
+    return num * word
 @app.route('/<path:path>')
 def cath_all(path):
     return "Sorry! No response. Try again."
